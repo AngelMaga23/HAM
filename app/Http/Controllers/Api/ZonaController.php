0e071e9c3c29c5a448e45e4334ff8780942ad228
Angel Magaña <angelmaga23@outlook.com>
2020-12-02 00:50:23 -0600

    Modificación de zonas
    Se cambió la consulta de Zonas

--- app/Http/Controllers/Api/ZonaController.php
+++ app/Http/Controllers/Api/ZonaController.php
@@ -18,12 +18,18 @@ class ZonaController extends Controller
         $zonas = "";
         try {
 
-            $zonas = DB::table('zonas as z')
-            ->select(DB::raw('z.id as idzona, z.nbZona,z.DesZonas, z.pathArchivo,z.numPersonasPermitidasMax, e.nbEstatus, e.tpEstatus, count(a.idZona) as cantAreas'))
-            ->join('estatus as e','z.idEstatus','=','e.id')
-            ->leftJoin('areas as a','z.id','=','a.idZona')
-            ->groupBy('a.idZona')
-            ->get();
+            $zonas = DB::table('zonas')
+                        ->select(DB::raw('zonas.id as idzona, zonas.nbZona, zonas.DesZonas, zonas.pathArchivo, zonas.numPersonasPermitidasMax, estatus.nbEstatus, estatus.tpEstatus,(SELECT COUNT(*) FROM areas WHERE areas.idZona = zonas.id) as cantAreas'))
+                        ->join('estatus','zonas.idEstatus','=','estatus.id')
+                        ->leftJoin('areas','zonas.id','=','areas.idZona')
+                        ->get();
+
+            // $zonas = DB::table('zonas as z')
+            // ->select(DB::raw('z.id as idzona, z.nbZona,z.DesZonas, z.pathArchivo,z.numPersonasPermitidasMax, e.nbEstatus, e.tpEstatus, count(a.idZona) as cantAreas'))
+            // ->join('estatus as e','z.idEstatus','=','e.id')
+            // ->leftJoin('areas as a','z.id','=','a.idZona')
+            // ->groupBy('a.idZona')
+            // ->get();
 
             if(!$zonas->isEmpty())
             {
